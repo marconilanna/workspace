5bab24f7329522f996ccf15a6ef87c74fd16d5f9
Marconi Lanna <marconi.lanna@gmail.com>
2012-09-19 00:49:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Installation Instructions
+
+Download the latest **Eclipse 3.8 Platform Runtime Binary** package from http://download.eclipse.org/eclipse/downloads/eclipse3x.html and extract it to your **Applications** folder.
+
+Copy the `plugins.sh` script and edit it if necessary. Run `./plugins.sh help` for options or `./plugins.sh install` to download and install the following features:
+
+* Java Development Tools
+* JavaScript Development Tools
+* Web Developer Tools
+* Mylyn
+* EGit
+* Scala IDE (2.1 M2 for Scala 2.9)
+
+**Optional**. Right-click **Eclipse.app** and select **Show Package Contents**. Navigate to **Contents**, **MacOS** and open the file **eclipse.ini**. Add the following after `-vmargs`:
+
+```
+-server
+-Xms100m
+-Xmx1500m
+-XX:MaxPermSize=256m
+-Dserver_port=1080
+-Declipse.p2.unsignedPolicy=allow
+```
---
+++ plugins.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#######
+# Shameless stolen from http://scala-ide.org/blog/director-script.html
+#
+# Additional information:
+# https://github.com/scala-ide/scala-ide/blob/master/eclipse-director.sh
+# http://help.eclipse.org/indigo/index.jsp?topic=/org.eclipse.platform.doc.isv/guide/p2_director.html
+
+eclipse_dir="/Applications/Eclipse/"
+eclipse_repo="http://download.eclipse.org/releases/juno/"
+scala_repo="http://download.scala-ide.org/releases-juno-29/milestone/site/"
+
+eclipse_opt="-nosplash"
+app="org.eclipse.equinox.p2.director"
+
+function usage()
+{
+    cat <<EOF
+usage: `basename $0` [options] command
+
+Options:
+    --eclipse-dir <path>     Path to your Eclipse installation
+                             Defaults to $eclipse_dir
+
+    --eclipse-repo <url>     Eclipse repository URL
+                             Defaults to $eclipse_repo
+
+    --scala-repo <url>       Scala IDE repository URL
+                             Defaults to $scala_repo
+
+Commands:
+    list                     List available plugins
+
+    install                  Install plugins
+
+    help                     Print this help screen
+EOF
+    exit 1
+}
+
+function install()
+{
+    echo "Installing from:"
+    echo " . $eclipse_repo"
+    echo " . $scala_repo"
+    echo
+    $eclipse_dir/eclipse $eclipse_opt \
+        -application $app \
+        -repository $eclipse_repo,$scala_repo \
+        -installIU \
+org.eclipse.jdt.feature.group,\
+org.eclipse.wst.jsdt.feature.feature.group,\
+org.eclipse.wst.web_ui.feature.feature.group,\
+org.eclipse.mylyn.ide_feature.feature.group,\
+org.eclipse.egit.feature.group,\
+org.scala-ide.sdt.feature.feature.group
+}
+
+while [ $# -gt 0 ]; do
+
+    case $1 in
+        "" | "help")
+            usage
+            ;;
+
+        "--eclipse-dir")
+            eclipse_dir=$2
+            echo "Eclipse installation dir is $eclipse_dir"
+            shift 2
+            ;;
+
+        "--eclipse-repo")
+            eclipse_repo=$2
+            echo "Eclipse repository is $eclipse_repo"
+            shift 2
+            ;;
+
+        "--scala-repo")
+            scala_repo=$2
+            echo "Scala IDE repository is $scala_repo"
+            shift 2
+            ;;
+
+        "list")
+            $eclipse_dir/eclipse $eclipse_opt \
+                -application $app \
+                -repository $eclipse_repo,$scala_repo \
+                -list \
+                | grep feature.group \
+                | awk -F "=" '{print $1}'
+            shift
+            ;;
+
+        "install")
+            install
+            shift
+            ;;
+
+        *)
+            usage
+            ;;
+    esac
+done
